aae86be36954d2de8d21134a764c08d46753cd42
Roger Oliveira <rogerpolvr@gmail.com>
2024-08-06 22:15:11 -0300

    feat: add some queries and database functions

--- internal/store/pgstore/migrations/002_create_messages_table.sql
+++ internal/store/pgstore/migrations/002_create_messages_table.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS messages (
     "reaction_count" BIGINT NOT NULL DEFAULT 0,
     "answered" BOOLEAN NOT NULL DEFAULT false,
     FOREIGN KEY (room_id) REFERENCES rooms(id)
-)
- ---- create above / drop below ----
+) 
+---- create above / drop below ----
 DROP TABLE IF EXISTS messages;
 
 -- Write your migrate down statements here. If this migration is irreversible
